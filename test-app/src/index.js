1cd03b8a781e061af59c2bfa3a2bc959c9bf9ea8
Manasi <manasipatilap2117@gmail.com>
2022-08-24 18:21:40 +0530

    exporting multiple values type 2

--- test-app/src/index.js
+++ test-app/src/index.js
@@ -1,15 +1,16 @@
 import ReactDom from 'react-dom'
 import './index.css'
-import course,{fees, text} from './App.js'
+// import course,{fees, text} from './App.js'
+import * as x from './App'
 ReactDom.render(
     <>
     <h1>React</h1>
     <ul>
         <li>Angular</li>
         <li>Mongo DB</li>
-        <li>{course}</li>
-        <li>{fees}</li>
-        <li>{text()}</li>
+        <li>{x.default}</li>
+        <li>{x.fees}</li>
+        <li>{x.text()}</li>
     </ul>
     </>,
     document.getElementById("root")
