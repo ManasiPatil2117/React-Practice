089d66ae5a075cd16a774af1b6bbaa93f74ac913
Manasi <manasipatilap2117@gmail.com>
2022-09-05 18:27:55 +0530

    Link in Routing

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,9 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
 import About from './About'
 import Contact from './Contact'
 function App() {
     return(
     <BrowserRouter>
+    <nav>
+        {/* <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact us</a> */}
+        {/* Instead of a href use LINK as it will loads the links very faster */}
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact Us</Link>
+    </nav>
         <Routes>
             <Route path="/" element={<div>Message</div>}></Route>
             <Route path="/about" element={<About/>}/>
