3bc5cf1a865795fa4002f2690f8e916da14ec4f7
Manasi <manasipatilap2117@gmail.com>
2022-09-05 18:48:20 +0530

    img as a link

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -4,15 +4,15 @@ import Contact from './Contact'
 function App() {
     return(
     <BrowserRouter>
-    <nav>
+    <>
         {/* <a href="/">Home</a>
         <a href="/about">About</a>
         <a href="/contact">Contact us</a> */}
         {/* Instead of a href use LINK as it will loads the links very faster */}
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact Us</Link>
-    </nav>
+        <Link to="/" className="link"><img src="./logo192.png" alt="" width={35}/></Link>
+        <Link to="/about" className="link">About</Link>
+        <Link to="/contact" className="link">Contact Us</Link>
+    </ >
         <Routes>
             <Route path="/" element={<div>Message</div>}></Route>
             <Route path="/about" element={<About/>}/>
