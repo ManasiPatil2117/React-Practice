23e8acf69bef4379d6d888331c2bae50cb3434bd
Manasi <manasipatilap2117@gmail.com>
2022-09-06 18:29:18 +0530

    States in class component

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,24 +1,17 @@
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
-import About from './About'
-import Contact from './Contact'
-function App() {
-    return(
-    <BrowserRouter>
-    <>
-        {/* <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact us</a> */}
-        {/* Instead of a href use LINK as it will loads the links very faster */}
-        <Link to="/" className="link"><img src="./logo192.png" alt="" width={35}/></Link>
-        <Link to="/about" className="link">About</Link>
-        <Link to="/contact" className="link">Contact Us</Link>
-    </ >
-        <Routes>
-            <Route path="/" element={<div>Message</div>}></Route>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/contact" element={<Contact/>}/>
-    </Routes>
-    </BrowserRouter >
-    )
+import React from "react"
+class App extends React.Component{
+    state = {
+        name : "Rahul",
+        place : "India"
+    }
+
+    render(){
+        return (
+            <>
+            <h1>Hello {this.state.name}</h1>
+            <h3>Place is {this.state.place}</h3>
+            </>
+        )
+    }
 }
 export default App
